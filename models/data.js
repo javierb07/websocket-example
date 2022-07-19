adb19d19f9c2be4a911cb09d1430fc574938cce1
Javier Belmonte <43077436+javierb07@users.noreply.github.com>
2022-07-19 17:20:54 -0300

    Formatting

--- models/data.js
+++ models/data.js
@@ -1,14 +1,10 @@
 var mongoose = require("mongoose");
 
 // Define schemas
-var entrySchema = new mongoose.Schema({
-    entry: Array
-});
 var dataSchema = new mongoose.Schema({
     state: Boolean,
     imu: [Array]
 
 });
 
-module.exports = mongoose.model("Entry", entrySchema);
 module.exports = mongoose.model("Data", dataSchema);
